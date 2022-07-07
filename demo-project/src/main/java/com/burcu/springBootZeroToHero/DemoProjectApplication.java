377f0d4b94fbac7989e56010ec05b5284afeb554
Nihat Alim <iletisim@nihatalim.com.tr>
2022-07-07 16:07:59 +0300

    Crud operation has been done

---
+++ demo-project/src/main/java/com/burcu/springBootZeroToHero/DemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.burcu.springBootZeroToHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("****");
+		SpringApplication.run(DemoProjectApplication.class, args);
+	}
+
+}
