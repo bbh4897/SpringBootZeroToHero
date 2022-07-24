758afb8857293bdb3048233484e7ee6e6e98829c
bbh4897 <42186003+bbh4897@users.noreply.github.com>
2022-07-24 14:19:42 +0300

    Validation has bee added with annotation

--- demo-project/src/main/java/com/burcu/springBootZeroToHero/service/UserService.java
+++ demo-project/src/main/java/com/burcu/springBootZeroToHero/service/UserService.java
@@ -3,6 +3,7 @@ package com.burcu.springBootZeroToHero.service;
 import com.burcu.springBootZeroToHero.exception.UserNotFoundException;
 import com.burcu.springBootZeroToHero.model.UserModel;
 import com.burcu.springBootZeroToHero.repository.UserRepository;
+import com.burcu.springBootZeroToHero.request.CreateUserRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,7 +22,11 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public UserModel addUser(UserModel userModel) {
+    public UserModel addUser(CreateUserRequest userRequest) {
+        UserModel userModel = new UserModel();
+        userModel.setUserName(userRequest.getUserName());
+        userModel.setPassword(userRequest.getPassword());
+        userModel.setEmail(userRequest.getEmail());
         return userRepository.save(userModel);
     }
 
